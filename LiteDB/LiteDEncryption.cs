92fe35b3f0dbfff0e29245fb6c92649822671da9
RainsSoft <andyhebear@163.com>
2015-10-28 13:20:37 +0800

    add encrypt/decrypt stream to support fileStorage save/read
    //encrypt
    if (db.FileStorage.Exists("10005/2.txt"))
    db.FileStorage.Delete("10005/2.txt");
    MemoryStream ms2 = new MemoryStream(buf2);
    LiteXorEncoderStream xe2 = new LiteXorEncoderStream(ms2);
    xe2.Initialize(null);
    db.FileStorage.Upload("10005/2.txt", xe2);
    ms2.Close();
    
    //decrypt
    MemoryStream ms22 = new MemoryStream();
    // Write file stream in a external stream
    LiteXorDecoderStream ed = new LiteXorDecoderStream(ms22);
    db.FileStorage.Download("10005/2.txt", ed);
    ms22.Position = 0L;
    byte[] buf222 = ms22.ToArray();

---
+++ LiteDB/LiteDEncryption.cs
@@ -0,0 +1,486 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace LiteDB
+{
+    /// <summary>
+    /// Interface for an encryption algorithm
+    /// </summary>
+    public abstract class LiteDEncryption
+    {
+
+        public void SetKey(string str) {
+            var bytes = System.Text.Encoding.ASCII.GetBytes(str);
+            SetKey(bytes, 0, bytes.Length);
+        }
+
+        public abstract void SetKey(byte[] data, int offset, int count);
+
+        /// <summary>
+        /// Encrypt an outgoing message in place
+        /// </summary>
+        public abstract bool Encrypt(byte[] buffer);
+        public abstract bool Encrypt(byte[] fileStream, int streamOffset);
+        public abstract bool Encrypt(byte[] fileStream, int streamOffset, int count);
+        /// <summary>
+        /// Decrypt an incoming message in place
+        /// </summary>
+        public abstract bool Decrypt(byte[] buffer);
+        public abstract bool Decrypt(byte[] fileStream, int streamOffset);
+        public abstract bool Decrypt(byte[] fileStream, int streamOffset, int count);
+    }
+    public class LiteXorEncryption : LiteDEncryption
+    {
+        private byte[] m_key;
+
+        /// <summary>
+        /// NetXorEncryption constructor
+        /// </summary>
+        public LiteXorEncryption(byte[] key)
+            : base() {
+            m_key = key;
+        }
+
+        public override void SetKey(byte[] data, int offset, int count) {
+            m_key = new byte[count];
+            Array.Copy(data, offset, m_key, 0, count);
+        }
+
+        /// <summary>
+        /// NetXorEncryption constructor
+        /// </summary>
+        public LiteXorEncryption(string key)
+            : base() {
+            m_key = Encoding.UTF8.GetBytes(key);
+        }
+
+        /// <summary>
+        /// Encrypt an outgoing message
+        /// </summary>
+        public override bool Encrypt(byte[] msg) {
+            int numBytes = msg.Length;
+            for (int i = 0; i < numBytes; i++) {
+                int offset = i % m_key.Length;
+                msg[i] = (byte)(msg[i] ^ m_key[offset]);
+            }
+            return true;
+        }
+
+        /// <summary>
+        /// Decrypt an incoming message
+        /// </summary>
+        public override bool Decrypt(byte[] msg) {
+            int numBytes = msg.Length;
+            for (int i = 0; i < numBytes; i++) {
+                int offset = i % m_key.Length;
+                msg[i] = (byte)(msg[i] ^ m_key[offset]);
+            }
+            return true;
+        }
+
+
+        public override bool Encrypt(byte[] fileStream, int streamOffset) {
+            int numBytes = fileStream.Length;
+            return Encrypt(fileStream,streamOffset,numBytes);
+        }
+        /// <summary>
+        /// 基于流的加密
+        /// </summary>
+        /// <param name="fileStream"></param>
+        /// <param name="streamOffset"></param>
+        /// <param name="count"></param>
+        /// <returns></returns>
+        public override bool Encrypt(byte[] fileStream, int streamOffset,int count) {
+            int numBytes = count;//fileStream.Length;
+            for (int i = 0; i < numBytes; i++) {
+                int offset = (streamOffset + i) % m_key.Length;
+                fileStream[i] = (byte)(fileStream[i] ^ m_key[offset]);
+            }
+            return true;
+        }
+
+        public override bool Decrypt(byte[] fileStream, int streamOffset) {
+            int numBytes = fileStream.Length;
+            return Decrypt(fileStream,streamOffset,numBytes);
+        }
+        /// <summary>
+        /// 基于流的解密
+        /// </summary>
+        /// <param name="fileStream">stream 的某段byte[]</param>
+        /// <param name="streamOffset">stream.position</param>
+        /// <param name="count"></param>
+        public override bool Decrypt(byte[] fileStream, int streamOffset, int count) {
+            int numBytes = count;//fileStream.Length; 
+            for (int i = 0; i < numBytes; i++) {
+                int offset = (streamOffset + i) % m_key.Length;
+                fileStream[i] = (byte)(fileStream[i] ^ m_key[offset]);
+            }
+            return true;
+        }
+    }
+    /// <summary>
+    /// 加密流
+    /// </summary>
+    /// <summary>
+    /// Represents an encoder stream for raw  streams.
+    /// </summary>
+    /// <remarks>
+    /// Note that this class does NOT handle/support any of the  container streams/headers.
+    /// </remarks>
+    public sealed class LiteXorEncoderStream : Stream
+    {
+        #region Fields
+
+        private readonly Stream stream;
+        private LiteXorEncryption encoder;
+
+        #endregion
+
+        #region Properties
+
+        /// <summary>
+        /// Gets a value indicating whether the stream supports reading.
+        /// Always returns false.
+        /// </summary>
+        public override bool CanRead {
+            get { return false; }
+        }
+
+        /// <summary>
+        /// Gets a value indicating whether the stream supports writing.
+        /// Always returns true.
+        /// </summary>
+        public override bool CanWrite {
+            get { return true; }
+        }
+
+        /// <summary>
+        /// Gets a value indicating whether the stream supports seeking.
+        /// Always returns false.
+        /// </summary>
+        public override bool CanSeek {
+            get { return false; }
+        }
+
+        /// <summary>
+        /// Gets or sets the position in the stream.
+        /// Not supported.
+        /// </summary>
+        public override long Position {
+            get { throw new NotSupportedException(); }
+            set { throw new NotSupportedException(); }
+        }
+
+        /// <summary>
+        /// Gets the length of the stream.
+        /// Not supported.
+        /// </summary>
+        public override long Length {
+            get { throw new NotSupportedException(); }
+        }
+
+        #endregion
+
+        #region Constructors
+
+        /// <summary>
+        /// Creates a new EncoderStream.
+        /// </summary>
+        public LiteXorEncoderStream(Stream stream) {
+            this.stream = stream;
+            this.encoder = new LiteXorEncryption("litedb");
+        }
+
+        #endregion
+
+        #region Methods
+
+        /// <summary>
+        /// Initializes the encoder.
+        /// </summary>
+        /// <param name="properties">The encoder properties.</param>
+        public void Initialize(LiteXorEncryption properties) {
+            //if (this.encoder == null || !this.encoder.Properties.Compare(properties))
+            //    this.encoder = new FastEncoder(this.stream, properties);
+            //this.encoder.Initialize();
+            encoder = properties;
+            if (encoder == null) {
+                encoder = new LiteXorEncryption("litedb");
+            }
+        }
+
+        /// <summary>
+        /// Reads data from the stream.
+        /// Not supported.
+        /// </summary>
+        /// <param name="buffer">The buffer to store the data.</param>
+        /// <param name="offset">The offset in the buffer at which data is written.</param>
+        /// <param name="count">The number of bytes to read.</param>
+        /// <returns>The number of bytes read.</returns>
+        public override int Read(byte[] buffer, int offset, int count) {
+            if (this.encoder == null)
+                throw new InvalidOperationException("Decoder is not initialized. Please initialize it using the \"Initialize\" method first.");
+
+            int oldpos = (int)this.stream.Position;
+            int numBytes = this.stream.Read(buffer, offset, count);
+            if (numBytes == 0)
+                return 0;
+            this.encoder.Encrypt(buffer, oldpos, numBytes);
+            //decodedBytes += numBytes;
+
+            return (int)numBytes;
+        }
+
+        /// <summary>
+        /// Writes data to the stream.
+        /// Not supported.
+        /// </summary>
+        /// <param name="buffer">The buffer written to the stream.</param>
+        /// <param name="offset">The offset in the buffer from where reading starts.</param>
+        /// <param name="count">The number of bytes to write.</param>
+        public override void Write(byte[] buffer, int offset, int count) {
+            throw new NotSupportedException();
+
+
+            //if (this.encoder == null)
+            //    throw new InvalidOperationException("Encoder is not initialized. Please initialize it using the \"Initialize\" method first.");
+            //int posold = (int)this.stream.Position;//原始流长度有限制
+            //int constBL = 1024 * 4;
+            //int bufsize = (count > constBL) ? constBL : count;
+            //byte[] buf = new byte[bufsize];
+            ////拆开
+            //int readc = count;
+            //while (readc > 0) {
+            //    int leaft = (readc > bufsize) ? bufsize : readc;
+            //    Array.Copy(buffer, offset, buf, 0, leaft);
+            //    encoder.Encrypt(buf, posold);//加密
+            //    this.stream.Write(buf, 0, leaft);
+            //    //
+            //    offset += leaft;
+            //    posold += leaft;
+            //    readc -= bufsize;
+            //}
+
+        }
+
+        /// <summary>
+        /// Seeks in the stream.
+        /// Not supported.
+        /// </summary>
+        /// <param name="offset">The seek offset.</param>
+        /// <param name="origin">The seek origin.</param>
+        /// <returns>The new position.</returns>
+        public override long Seek(long offset, SeekOrigin origin) {
+            throw new NotSupportedException();
+        }
+
+        /// <summary>
+        /// Flushes the stream.
+        /// </summary>
+        public override void Flush() {
+            //this.encoder.Encode(true);
+        }
+
+        /// <summary>
+        /// Closes the stream and the encoder.
+        /// </summary>
+        public override void Close() {
+            //this.encoder.Close();
+        }
+
+        /// <summary>
+        /// Sets the length of the stream. Not supported.
+        /// </summary>
+        /// <param name="value"></param>
+        public override void SetLength(long value) {
+            throw new NotSupportedException();
+        }
+
+        #endregion
+    }
+
+    /// <summary>
+    /// Represents a decoder stream for raw  streams.
+    /// </summary>
+    /// <remarks>
+    /// Note that this class does NOT handle/support any of the  container streams/headers.
+    /// </remarks>
+    public sealed class LiteXorDecoderStream : Stream
+    {
+        #region Fields
+
+        private readonly Stream stream;
+        private LiteXorEncryption decoder;
+        private long decodedBytes;
+        private long decodedSize;
+
+        #endregion
+
+        #region Properties
+
+        /// <summary>
+        /// Gets a value indicating whether the stream supports reading.
+        /// Always returns true.
+        /// </summary>
+        public override bool CanRead {
+            get { return true; }
+        }
+
+        /// <summary>
+        /// Gets a value indicating whether the stream supports writing.
+        /// Always returns false.
+        /// </summary>
+        public override bool CanWrite {
+            get { return false; }
+        }
+
+        /// <summary>
+        /// Gets a value indicating whether the stream supports seeking.
+        /// Always returns false.
+        /// </summary>
+        public override bool CanSeek {
+            get { return false; }
+        }
+
+        /// <summary>
+        /// Gets or sets the position in the stream.
+        /// Not supported.
+        /// </summary>
+        public override long Position {
+            get { throw new NotSupportedException(); }
+            set { throw new NotSupportedException(); }
+        }
+
+        /// <summary>
+        /// Gets the length of the stream.
+        /// This returns the length of the decoded data.
+        /// If the size of the decoded data is unknown, a negative value is returned.
+        /// </summary>
+        public override long Length {
+            get { return this.stream.Length; }
+        }
+
+        #endregion
+
+        #region Constructors
+
+        /// <summary>
+        /// Creates a new DecoderStream.
+        /// </summary>
+        public LiteXorDecoderStream(Stream stream) {
+            this.stream = stream;
+            this.decoder = new LiteXorEncryption("litedb");
+        }
+
+        #endregion
+
+        #region Methods
+
+        /// <summary>
+        /// Initializes the decoder.
+        /// </summary>
+        /// <param name="properties">The decoder properties.</param>
+        /// <remarks>
+        /// Please note that this also reads data from the stream in order to initialize the range decoder.
+        /// </remarks>
+        public void Initialize(LiteXorEncryption properties) {
+            //if (this.decoder == null || !this.decoder.Properties.Compare(properties))
+            //    this.decoder = new Decoder(this.stream, properties);
+
+            //this.decoder.Initialize();
+            this.decoder = properties;
+            if (this.decoder == null) {
+                this.decoder = new LiteXorEncryption("litedb");
+            }
+            this.decodedSize = this.stream.Length;
+        }
+
+        /// <summary>
+        /// Reads and decodes data from the stream.
+        /// </summary>
+        /// <param name="buffer">The buffer to store the decoded data.</param>
+        /// <param name="offset">The offset in the buffer at which decoded data is written.</param>
+        /// <param name="count">The number of bytes to decode.</param>
+        /// <returns>The number of decoded bytes.</returns>
+        public override int Read(byte[] buffer, int offset, int count) {
+            throw new NotSupportedException();
+
+            //if (this.decoder == null)
+            //    throw new InvalidOperationException("Decoder is not initialized. Please initialize it using the \"Initialize\" method first.");
+
+            //int oldpos = (int)this.stream.Position;
+            //int numBytes = this.stream.Read(buffer, offset, count);
+            //if (numBytes == 0)
+            //    return 0;
+            //this.decoder.Decrypt(buffer, oldpos);
+            //decodedBytes += numBytes;
+
+            //return (int)numBytes;
+        }
+
+        /// <summary>
+        /// Writes data to the stream.
+        /// Not supported.
+        /// </summary>
+        /// <param name="buffer">The buffer written to the stream.</param>
+        /// <param name="offset">The offset in the buffer from where reading starts.</param>
+        /// <param name="count">The number of bytes to write.</param>
+        public override void Write(byte[] buffer, int offset, int count) {
+            
+            if (this.decoder == null)
+                throw new InvalidOperationException("Encoder is not initialized. Please initialize it using the \"Initialize\" method first.");
+            int posold = (int)this.stream.Position;//原始流长度有限制
+            int constBL = 1024 * 4;
+#if DEBUG
+            constBL = 11;
+#endif
+            int bufsize = (count > constBL) ? constBL : count;
+            byte[] buf = new byte[bufsize];
+            //拆开
+            int readc = count;
+            while (readc > 0) {
+                int leaft = (readc > bufsize) ? bufsize : readc;
+                Array.Copy(buffer, offset, buf, 0, leaft);
+                decoder.Decrypt(buf, posold,leaft);//加密
+                this.stream.Write(buf, 0, leaft);
+                //
+                offset += leaft;
+                posold += leaft;
+                readc -= bufsize;
+            }
+        }
+
+        /// <summary>
+        /// Seeks in the stream.
+        /// Not supported.
+        /// </summary>
+        /// <param name="offset">The seek offset.</param>
+        /// <param name="origin">The seek origin.</param>
+        /// <returns>The new position.</returns>
+        public override long Seek(long offset, SeekOrigin origin) {
+            throw new NotSupportedException();
+        }
+
+        /// <summary>
+        /// Flushes the stream.
+        /// </summary>
+        public override void Flush() {
+
+        }
+
+        /// <summary>
+        /// Sets the length of the decoded data.
+        /// Not supported.
+        /// </summary>
+        /// <param name="value"></param>
+        public override void SetLength(long value) {
+            //this.decodedSize = value;
+            throw new NotSupportedException();
+        }
+
+        #endregion
+    }
+}
--- LiteDB_Test/Form1.cs
+++ LiteDB_Test/Form1.cs
@@ -19,15 +19,16 @@ namespace LiteDB_Test
         }
 
         private void Form1_Load(object sender, EventArgs e) {
+            test2();
             Stopwatch sw = new Stopwatch();
             sw.Start();
             for (int i = 0; i < 10; i++) {
-                test1();
+                //test1();
             }
             sw.Stop();
             Console.WriteLine(sw.ElapsedMilliseconds);
         }
-        
+
         private static void test1() {
             // Open database (or create if not exits)
             using (var db = new LiteDatabase(Application.StartupPath + "/MyData.db")) {
@@ -43,29 +44,29 @@ namespace LiteDB_Test
                 }
                 catch (Exception ee) {
                     MessageBox.Show(ee.ToString());
-                }                
+                }
                 // Create your new customer instance
                 var customer = new Customer {
                     //ID是对象默认属性，新增加时候必须设置0
-                    Id=0,
+                    Id = 0,
                     Name = "John Doe",
                     Phones = new string[] { "8000-0000", "9000-0000" },
                     IsActive = true,
-                    mF=cf
+                    mF = cf
                 };
 
                 // Insert new customer document (Id will be auto-incremented)
                 col.Insert(customer);
 
                 // Update a document inside a collection
-                customer.Name = "Joana Doe_"+(cf/10).ToString();
+                customer.Name = "Joana Doe_" + (cf / 10).ToString();
 
                 col.Update(customer);
 
                 // Index document using a document property
                 col.EnsureIndex(x => x.Name);
                 //other io test
-                customer.Name = "Job test"+cf.ToString();
+                customer.Name = "Job test" + cf.ToString();
                 LiteDB_Test.LiteDBHelper.Update<Customer>(db, "customers", customer);
                 Customer c2 = customer.clone();
                 c2.Id = 0;
@@ -76,9 +77,9 @@ namespace LiteDB_Test
                 var results = col.Find(x => x.Name.StartsWith("Jo"));
                 foreach (var v in results) {
                     //Debug.Log(v.Name);
-                    Console.WriteLine("name:"+v.Name+"_mf:"+v.mF.ToString());
+                    Console.WriteLine("name:" + v.Name + "_mf:" + v.mF.ToString());
                 }
-              
+
 
             }
         }
@@ -86,37 +87,61 @@ namespace LiteDB_Test
         void test2() {
             LiteDB.LiteDatabase db = new LiteDatabase(Application.StartupPath + "/test.nosql");
             byte[] buf = new byte[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            byte[] buf2 = new byte[16];
+            for (int i = 0; i < buf2.Length; i++) {
+                buf2[i] = (byte)(i % 255);
+            }
+            byte[] buf3 = new byte[25];
+            for (int i = 0; i < buf3.Length; i++) {
+                buf3[i] = (byte)(i % 255);
+            }
             using (MemoryStream ms = new MemoryStream(buf)) {
                 //db.BeginTrans();
                 try {
+                    //1
                     ms.Position = 0;
+                    LiteXorEncoderStream xe = new LiteXorEncoderStream(ms);
+                    xe.Initialize(null);
                     if (db.FileStorage.Exists("10005/1.txt")) db.FileStorage.Delete("10005/1.txt");
-                    db.FileStorage.Upload("10005/1.txt", ms);
-                    ms.Position = 0;
+                    db.FileStorage.Upload("10005/1.txt", xe);
+                    //2
+                    //ms.Position = 0;
                     if (db.FileStorage.Exists("10005/2.txt")) db.FileStorage.Delete("10005/2.txt");
-                    db.FileStorage.Upload("10005/2.txt", ms);
+                    MemoryStream ms2 = new MemoryStream(buf2);
+                    LiteXorEncoderStream xe2 = new LiteXorEncoderStream(ms2);
+                    xe2.Initialize(null);
+                    db.FileStorage.Upload("10005/2.txt", xe2);
+                    ms2.Close();
+                    //3
                     if (db.FileStorage.Exists("10005/3.txt")) db.FileStorage.Delete("10005/3.txt");
-                    db.FileStorage.Upload("10005/3.txt", ms);
+                    MemoryStream ms3 = new MemoryStream(buf3);
+                    LiteXorEncoderStream xe3 = new LiteXorEncoderStream(ms3);
+                    xe3.Initialize(null);
+                    db.FileStorage.Upload("10005/3.txt", xe3);
+                    //4
                     if (db.FileStorage.Exists("10005/4.txt")) db.FileStorage.Delete("10005/4.txt");
                     db.FileStorage.Upload("10005/4.txt", ms);
 
-                    // Get file reference using file id
-                    var file = db.FileStorage.FindById("10005/1.txt");
 
                     // Find all files using StartsWith
                     IEnumerable<LiteFileInfo> files = db.FileStorage.Find("10005/"); //db.Files.Find("my_");
-
                     foreach (var vf in files) {
                         Console.WriteLine(vf.Id + "     " + vf.Filename);
                         //Debug.Log(vf.Id + "     " + vf.Filename);
                     }
+
+
+                    // Get file reference using file id
+                    var file = db.FileStorage.FindById("10005/1.txt");
                     // Get file stream
                     var stream = file.OpenRead();
                     stream.Read(buf, 0, buf.Length);
-                    MemoryStream ms2 = new MemoryStream();
+                    MemoryStream ms22 = new MemoryStream();
                     // Write file stream in a external stream
-                    db.FileStorage.Download("10005/2.txt", ms2);
-                    byte[] buf2 = ms2.ToArray();
+                    LiteXorDecoderStream ed = new LiteXorDecoderStream(ms22);
+                    db.FileStorage.Download("10005/2.txt", ed);
+                    ms22.Position = 0L;
+                    byte[] buf222 = ms22.ToArray();
                     db.FileStorage.Delete("10005/1.txt");
                     //db.Commit();
                 }
